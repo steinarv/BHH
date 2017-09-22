e98f96a8110b481ec01321d9f255b87e9e924150
steinarv <steinar.veka@gmail.com>
2017-09-22 13:47:56 +0200

    ettersp

--- src/ettersp1.js
+++ src/ettersp1.js
@@ -10,17 +10,49 @@ var itemList = [ {name: "Kano", mInPrice: 2000, outPrice0: 10000, outPrice1: 10}
 
 var orderList = [];
 var inventoryList = [];
+var bankValue = 100000;
+
+var nxtBtn = document.getElementById("nxtBtn");
 
 var numericInput = document.createElement("INPUT");
 numericInput.setAttribute("type", "number");
 numericInput.setAttribute("value", "0");
 
+findItemInInventoryList = function(itemName) {
+    for(var i = 0; i < inventoryList.length; i++) {
+      if(inventoryList[i]['name'] === itemName) {
+        return i;
+      }
+    }
+    return(-1);
+}
 
 function makeOrderList() {
     orderList = getRandomSubarray(itemList, 3);
+    // Herer we can alter porperties
+}
+
+function purchFunc(){
+  var orderCountArr = document.getElementsByName("purchaseCount");
+  var totalCost = 0;
+  for(var i = 0; i < orderCountArr.length; i++){
+    totalCost += orderList[i]['mInPrice'] * orderCountArr[i].value;
+  }
+
+  if(totalCost <= bankValue){
+    document.getElementById("pBank").innerHTML = "Du har "
+    + (bankValue - totalCost).toLocaleString() + "kr å handle for";
+    nxtBtn.disabled = false;
+  }else{
+    document.getElementById("pBank").innerHTML = "Dette har du ikke råd til...";
+    nxtBtn.disabled = true;
+  }
+
 }
 
 function onLoadFunc() {
+  document.getElementById("pBank").innerHTML = "Du har " + bankValue.toLocaleString() + "kr å handle for";
+
   makeOrderList();
   var tbl = document.getElementById("tbOrders");
 
@@ -32,15 +64,62 @@ function onLoadFunc() {
     rw.insertCell(-1);
 
     var tmpInp = numericInput.cloneNode(true);
-    tmpInp.setAttribute("id", "purchase" + i);
+    tmpInp.setAttribute("id", "o_" + orderList[i]['name']);
+    tmpInp.setAttribute("oninput", "purchFunc()");
+    tmpInp.setAttribute("name", "purchaseCount");
     rw.insertCell(-1).appendChild(tmpInp);
 
-    var tmpInp = numericInput.cloneNode(true);
-    tmpInp.setAttribute("id", "salesprice" + i);
-    tmpInp.setAttribute("value", (orderList[i]['mInPrice'] * 1.5));
-    rw.insertCell(-1).appendChild(tmpInp);
-    /*for(var j = 0; j < n; j++) {
-      var cl = rw.insertCell(-1);
-    }*/
   }
+
+}
+
+buyFunc = function() {
+
+  // update inventoryList
+  var itemName, n, invInd;
+
+  var orderCountArr = document.getElementsByName("purchaseCount");
+  for(var i = 0; i < orderCountArr.length; i++){
+
+    n = parseInt(orderCountArr[i].value);
+    if(orderCountArr[i].value > 0) {
+      //check if item allready is in storage
+      itemName = orderCountArr[i].id.substr(2);
+      invInd = findItemInInventoryList(itemName);
+
+      //console.log(itemName + " ---- " + invInd);
+      if(invInd >= 0){
+        inventoryList[invInd]['n'] += n;
+      }else{
+        inventoryList.push( {name: itemName, n: n} );
+      }
+    }
+  }
+
+  // update table
+  var tbl = document.getElementById("tbInventory");
+  trs = tbl.getElementsByTagName('tr');
+
+  for(i = 1; i < trs.length; i++)tbl.deleteRow(-1);
+
+  for(i = 0; i < inventoryList.length; i++) {
+    n = parseInt(inventoryList[i]['n']);
+    if(n > 0){
+      rw = tbl.insertRow(-1);
+
+      rw.insertCell(-1).innerHTML = itemList[i]['name'];
+      rw.insertCell(-1).innerHTML = n.toString();
+
+      var tmpInp = numericInput.cloneNode(true);
+      tmpInp.setAttribute("id", "s_" + orderList[i]['name']);
+      tmpInp.setAttribute("name", "salesPrice");
+      rw.insertCell(-1).appendChild(tmpInp);
+    }
+  }
+
+
+}
+
+nxtFunc = function() {
+  var i = 3;
 }
